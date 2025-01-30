28763d980e4526b1dcfe9a8861c5d886ef9051c7
ing-lp-tech <ing.lp.tech@gmail.com>
2025-01-30 15:54:07 -0300

    first commit

---
+++ app/(dashboard)/rollos/new/page.tsx
@@ -0,0 +1,7 @@
+import RollosForm from "@/components/rollos/RollosForm";
+
+const CreateCollection = () => {
+  return <RollosForm />;
+};
+
+export default CreateCollection;
---
+++ components/rollos/RollosForm.tsx
@@ -0,0 +1,207 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+
+import { Separator } from "../ui/separator";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "../ui/textarea";
+import ImageUpload from "../custom ui/ImageUpload";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import Delete from "../custom ui/Delete";
+
+const formSchema = z.object({
+  tipo_de_tela: z.string().min(2).max(20),
+  color: z.string().min(2).max(500).trim(),
+  metros: z.string().min(2).max(500).trim(),
+  kgs: z.string().min(2).max(500).trim(),
+  image: z.string().optional(),
+});
+
+interface RolloFormProps {
+  initialData?: RolloType | null; //Must have "?" to make it optional
+}
+
+const RollosForm: React.FC<RolloFormProps> = ({ initialData }) => {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+      ? initialData
+      : {
+          tipo_de_tela: "",
+          color: "",
+          metros: "",
+          kgs: "",
+          image: "",
+        },
+  });
+
+  const handleKeyPress = (
+    e:
+      | React.KeyboardEvent<HTMLInputElement>
+      | React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      const url = initialData
+        ? `/api/rollos/${initialData._id}`
+        : "/api/rollos";
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(values),
+      });
+      if (res.ok) {
+        setLoading(false);
+        toast.success(`Rollos ${initialData ? "updated" : "created"}`);
+        window.location.href = "/rollos";
+        router.push("/rollos");
+      }
+    } catch (err) {
+      console.log("[rollos_POST]", err);
+      toast.error("Something went wrong! Please try again.");
+    }
+  };
+
+  return (
+    <div className="p-10">
+      {initialData ? (
+        <div className="flex items-center justify-between">
+          <p className="text-heading2-bold">Edit Collection</p>
+          <Delete id={initialData._id} item="collection" />
+        </div>
+      ) : (
+        <p className="text-heading2-bold">Ingreso de rollo</p>
+      )}
+      <Separator className="bg-grey-1 mt-4 mb-7" />
+      <Separator className="bg-grey-1 mt-4 mb-7" />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="tipo_de_tela"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tipo de tela</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Title"
+                    {...field}
+                    onKeyDown={handleKeyPress}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="color"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Color</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    rows={5}
+                    onKeyDown={handleKeyPress}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="metros"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Metros</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    rows={5}
+                    onKeyDown={handleKeyPress}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="kgs"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Kgs</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    rows={5}
+                    onKeyDown={handleKeyPress}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex gap-10">
+            <Button type="submit" className="bg-blue-1 text-white">
+              Submit
+            </Button>
+            <Button
+              type="button"
+              onClick={() => router.push("/collections")}
+              className="bg-blue-1 text-white"
+            >
+              Discard
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default RollosForm;
